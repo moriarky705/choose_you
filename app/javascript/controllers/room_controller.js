e4bc9f5f1c3cb277c82c0c30083d08c5f0ebcf00
moriarky705 <morinosin022@gmail.com>
2026-02-17 00:44:47 +0900

    Change selection result icon from star to cat

--- app/javascript/controllers/room_controller.js
+++ app/javascript/controllers/room_controller.js
@@ -238,17 +238,15 @@ class SelectionRenderer {
     return selected.map((p, index) => 
       `<div class="flex items-center p-4 bg-amber-50 border border-amber-200 rounded-lg">
         <div class="w-10 h-10 bg-amber-600 rounded-full flex items-center justify-center text-white font-bold mr-3">
-          ${this.renderStarIcon()}
+          ${this.renderCatIcon()}
         </div>
         <span class="text-gray-900 font-semibold">${this.escapeHtml(p.name)}</span>
       </div>`
     ).join('')
   }
 
-  renderStarIcon() {
-    return `<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.196-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path>
-    </svg>`
+  renderCatIcon() {
+    return `<span class="text-xl">🐱</span>`
   }
 
   escapeHtml(unsafe) {
